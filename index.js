c6361257f04a05ccd955346bb95d4d6e82c1b1c1
Denys Bashkatov <denys.bashkatov@seetree.co>
2024-03-21 10:37:22 +0200

    All http methods support

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const { readFileSync } = require('fs');
+const {readFileSync} = require('fs');
 const superagent = require('superagent');
 
 const fetchResource = async (resource) => {
   const p = new Promise((resolve, reject) => {
     const handleResponse = (err, res) => {
       const rawRes= err ? err.response : res;
+      if (rawRes) {
         if (resource.expect) {
           if (resource.expect.status && rawRes.statusCode !== resource.expect.status) {
             reject(`Status code does not match for ${resource.url}, expected ${resource.expect.status} but got ${rawRes.statusCode}`);
           } else if (resource.expect.headers) {
             const headers = resource.expect.headers;
-            for (let i = 0; i < headers.length; i++) {
-              const header = headers[i];
-              if (rawRes.header[header.name] !== header.value) {
-                reject(`Header ${header.name} does not match for ${resource.url}, expected ${header.value} but got ${rawRes.header[header.name]}`);
+            const headerNames = Object.keys(resource.expect.headers);
+            for (let i = 0; i < headerNames.length; i++) {
+              const headerName = headerNames[i];
+              const checkHeaderName = headerName.toLowerCase();
+              const expectedValue = headers[headerName];
+
+              if (rawRes.header[checkHeaderName] !== expectedValue) {
+                const actualHeader = rawRes.header[checkHeaderName];
+                reject(`Header "${headerName}" does not match for ${resource.url}, expected ${expectedValue} but got ${actualHeader}`);
               }
             }
             resolve(`Fetched ${resource.url} with status code ${rawRes.statusCode}`);
@@ -23,11 +29,22 @@ const fetchResource = async (resource) => {
         } else {
           resolve(`Fetched ${resource.url} with status code ${rawRes.statusCode}`);
         }
+      } else {
+        reject(`Failed to fetch ${resource.url}`);
+      }
     };
-    superagent
-      .get(resource.url)
-      .redirects(resource.redirects || 0)
-      .end(handleResponse);
+    if (resource.method === 'POST' || resource.method === 'PUT' || resource.method === 'PATCH' || resource.method === 'DELETE') {
+      const method = resource.method.toLowerCase();
+      superagent[method](resource.url)
+          .send(resource.body)
+          .redirects(resource.redirects || 0)
+          .end(handleResponse);
+    } else if (resource.method === 'GET') {
+      superagent
+          .get(resource.url)
+          .redirects(resource.redirects || 0)
+          .end(handleResponse);
+    }
   });
   return p;
 };
@@ -49,26 +66,23 @@ const configContents = readFileSync(agruments.config, 'utf8');
 (async () => {
   try {
     const config = JSON.parse(configContents);
-    if (config.resources && config.resources.length && config.interval) {
-      const interval = config.interval * 1000;
+    if (config.resources && config.resources.length) {
       const resources = config.resources;
-      setInterval(async () => {
-        let message = '';
-        for (let i = 0; i < resources.length; i++) {
-          const resource = resources[i];
-          try {
-            const r = await fetchResource(resource);
-            message += r + '\n';
-          } catch (e) {
-            message += e + '\n';
-          }
-        };
-        console.log(message);
-      }, interval);
+      let message = '';
+      for (let i = 0; i < resources.length; i++) {
+        const resource = resources[i];
+        try {
+          const r = await fetchResource(resource);
+          message += r + ' \u2705' + '\n';
+        } catch (e) {
+          message += e + ' \u2717' + '\n';
+        }
+      };
+      console.log(message);
     } else {
       throw new Error('Config file is invalid');
     }
   } catch (e) {
     throw new Error('Config file is invalid');
   }
-})()
+})();
